d6dbb6bef7bf92259a2fd4c5d85c0d89f2d7db86
Youngmin Cho <103130581+Youngmin322@users.noreply.github.com>
2025-08-04 18:52:42 +0900

    feat: MultipeerConnectivity 전송을 위해 CardModel Codable 적용

--- WeLink/Models/CardModel.swift
+++ WeLink/Models/CardModel.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftData
 
 @Model
-class CardModel {
+class CardModel: Codable {
     @Attribute(.unique) var id: UUID
     var name: String
     var age: Int
@@ -19,7 +19,11 @@ class CardModel {
     var tag: String
     var dDay: Int
     var imageData: Data
-    
+
+    enum CodingKeys: String, CodingKey {
+        case id, name, age, cardDescription, birthDate, mbti, tag, dDay, imageData
+    }
+
     init(id: UUID = UUID(), name: String, age: Int, description: String, birthDate: String, mbti: String, tag: String, dDay: Int, imageData: Data) {
         self.id = id
         self.name = name
@@ -30,5 +34,31 @@ class CardModel {
         self.tag = tag
         self.dDay = dDay
         self.imageData = imageData
+    }
+
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(UUID.self, forKey: .id)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.age = try container.decode(Int.self, forKey: .age)
+        self.cardDescription = try container.decode(String.self, forKey: .cardDescription)
+        self.birthDate = try container.decode(String.self, forKey: .birthDate)
+        self.mbti = try container.decode(String.self, forKey: .mbti)
+        self.tag = try container.decode(String.self, forKey: .tag)
+        self.dDay = try container.decode(Int.self, forKey: .dDay)
+        self.imageData = try container.decode(Data.self, forKey: .imageData)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+        try container.encode(age, forKey: .age)
+        try container.encode(cardDescription, forKey: .cardDescription)
+        try container.encode(birthDate, forKey: .birthDate)
+        try container.encode(mbti, forKey: .mbti)
+        try container.encode(tag, forKey: .tag)
+        try container.encode(dDay, forKey: .dDay)
+        try container.encode(imageData, forKey: .imageData)
     }
 }
