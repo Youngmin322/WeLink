330c821386c01de10860c68fd27cd772275f4707
kwangmo24 <kwangmo24@postech.ac.kr>
2025-08-05 15:16:19 +0900

    Category 상단 UI
    .

--- WeLink/Views/CategoryView.swift
+++ WeLink/Views/CategoryView.swift
@@ -11,7 +11,11 @@ struct CategoryView: View {
     var progress: CGFloat
     
     var body: some View {
-        VStack{
+        ZStack{
+            Color("BackgroundColor")
+                .ignoresSafeArea()
+            VStack{
+                // 상단바
                 ZStack(alignment: .leading) {
                     let barWidth: CGFloat = 324
                     let barHeight: CGFloat = 2
@@ -20,14 +24,46 @@ struct CategoryView: View {
                     RoundedRectangle(cornerRadius: 4)
                         .fill(Color.gray.opacity(0.4))
                         .frame(width: barWidth, height: barHeight)
-
+                    
                     // 연두색 프로그레스 바
                     RoundedRectangle(cornerRadius: 4)
                         .fill(Color("MainColor"))
                         .frame(width: barWidth * progress, height: barHeight)
                 }
+                .padding(.bottom, 20)
                 
-                .frame(height: 8) // 고정 높이
+                //뒤로가기 버튼
+                Button(action: {
+                    //TODO: View 이동 action 추가하기
+                }) {
+                    Image(systemName: "chevron.left")
+                        .font(.system(size: 28))
+                        .frame(width: 330, alignment: .leading)
+                        .foregroundColor(Color("MainColor"))
+                }
+                .padding(.bottom, 20)
+    
+                
+                // 상단 텍스트
+                VStack(){
+                    let textWidth:CGFloat = 324
+                    
+                        Text("요즘 어떤 것에 관심이 있으신가요?\n카테고리를 선택해주세요.")
+                        .frame(width: textWidth, alignment: .leading)
+                            .foregroundColor(.white)
+                            .font(.system(size:22, weight: .bold))
+                            .padding(.bottom, 2)
+                    
+                    Text("최대 3개까지 선택해주세요.")
+                        .frame(width: textWidth, alignment: .leading)
+                        .foregroundColor(Color("MainColor"))
+                        .font(.system(size: 16, weight: .semibold))
+                    
+                    
+                }
+                
+                
+            }
         }
     }
 }
