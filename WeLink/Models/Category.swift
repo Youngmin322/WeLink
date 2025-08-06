e2360cd8186e0c38aa03b0b0e35417c7a1e6f253
kwangmo24 <kwangmo24@postech.ac.kr>
2025-08-06 14:57:01 +0900

    main topic view complete

--- WeLink/Models/Category.swift
+++ WeLink/Models/Category.swift
@@ -4,37 +4,42 @@
 //
 //  Created by 럭스(양광모) on 8/4/25.
 //
+import SwiftUI
+
+var mainTopicsOrder: [String] = ["패션", "여가", "스포츠", "영화", "맛집탐방", "뷰티", "여행", "독서", "반려동물", "게임", "식물", "문화생활"]
 
 var topicTitlesDict: [String: [String: [String]]] = [
-    "패션": [
+    "패션 👚": [
         "스타일": ["미니멀", "클래식", "스트릿", "캐주얼", "빈티지", "러블리", "Y2K", "젠더리스", "긱시크", "힙한", "스포티", "심플"],
         "아이템": ["가방", "신발", "모자", "시계", "주얼리", "벨트", "헤어소품", "키링", "아이웨어"],
         "취향 포인트": ["무채색", "컬러풀", "트렌디", "편안함", "패턴", "브랜드", "그래픽"]
         ],
-    "여가": ["활동": ["영화", "독서", "전시회", "음악", "뮤지컬", "연극", "산책", "요리", "드라이브", "다꾸", "혼술"],
+    "여가 🛋️": ["활동": ["영화", "독서", "전시회", "음악", "뮤지컬", "연극", "산책", "요리", "드라이브", "다꾸", "혼술"],
            "휴식 스타일": ["집순이", "밖순이", "기록형", "손활동", "힐링 지향", "콘텐츠", "숙면", "만들기"],
            "아이템": ["조명", "향초", "커피", "스피커", "티", "술잔", "책", "요리도구"]
            ],
-    "스포츠": [:],
-    "영화": [:],
-    "맛집탐방": [:],
-    "뷰티": [:],
-    "여행": [:],
-    "독서": [:],
-    "반려동물": [:],
-    "게임": [:],
-    "식물": [:],
-    "문화생활": [:]
+    "스포츠 ⚽️": [:],
+    "영화 📹": [:],
+    "맛집탐방 🍝": [:],
+    "뷰티 💄": [:],
+    "여행 ✈️": [:],
+    "독서 📖": [:],
+    "반려동물 🐶": [:],
+    "게임 👾": [:],
+    "식물 🌱": [:],
+    "문화생활 🎤": [:]
 ]
 
 
 // .mainTopics['패션'].children['스타일'].children['미니멀']
-class category{
-    var mainTopics: [String: mainTopic]
+class Category: ObservableObject{
+    @Published var mainTopics: [String: mainTopic]
+    var mainTopicsList: [String]
     var isSelected: Bool
     
     init() {
         self.mainTopics = [:]
+        self.mainTopicsList = mainTopicsOrder
         self.isSelected = false
         
         for (mainTitle, subTitleDict) in topicTitlesDict {
@@ -46,7 +51,9 @@ class category{
                 }
                 subTopicDict[subTitle] = subTopic(title: subTitle, children: detailedTopicDict)
             }
-            self.mainTopics[mainTitle] = mainTopic(title: mainTitle, children: subTopicDict)
+            let title:String = String(mainTitle.split(separator: " ")[0])
+            let emoji:String = String(mainTitle.split(separator: " ")[1])
+            self.mainTopics[title] = mainTopic(title: title, emoji: emoji, children: subTopicDict)
         }
     }
     
@@ -56,13 +63,15 @@ class category{
     
 }
 
-class mainTopic{
+class mainTopic: Identifiable, ObservableObject{
     let title: String
+    let emoji: String
     var children: [String:subTopic]
-    var isSelected: Bool
+    @Published var isSelected: Bool
     
-    init(title: String, children: [String:subTopic]) {
+    init(title: String, emoji: String, children: [String:subTopic]) {
         self.title = title
+        self.emoji = emoji
         self.children = children
         self.isSelected = false
     }
--- WeLink/Views/CategoryView.swift
+++ WeLink/Views/CategoryView.swift
@@ -9,8 +9,17 @@ import SwiftUI
 
 struct CategoryView: View {
     var progress: CGFloat
+    let categories: Category = Category()
+    @State var selectedTopics: [mainTopic] = []
+    @State var isReady: Bool = false
     
     var body: some View {
+        
+        let columns = [
+            GridItem(.flexible()),
+            GridItem(.flexible())
+        ]
+        
         ZStack{
             Color("BackgroundColor")
                 .ignoresSafeArea()
@@ -42,32 +51,104 @@ struct CategoryView: View {
                         .foregroundColor(Color("MainColor"))
                 }
                 .padding(.bottom, 20)
-    
+                
                 
                 // 상단 텍스트
                 VStack(){
                     let textWidth:CGFloat = 324
                     
-                        Text("요즘 어떤 것에 관심이 있으신가요?\n카테고리를 선택해주세요.")
+                    Text("요즘 어떤 것에 관심이 있으신가요?\n카테고리를 선택해주세요.")
                         .frame(width: textWidth, alignment: .leading)
-                            .foregroundColor(.white)
-                            .font(.system(size:22, weight: .bold))
-                            .padding(.bottom, 2)
+                        .foregroundColor(.white)
+                        .font(.system(size:22, weight: .bold))
+                        .padding(.bottom, 2)
                     
                     Text("최대 3개까지 선택해주세요.")
                         .frame(width: textWidth, alignment: .leading)
                         .foregroundColor(Color("MainColor"))
                         .font(.system(size: 16, weight: .semibold))
-                    
-                    
                 }
+                .padding(.bottom)
                 
-                
+                ScrollView{
+                    let categoryWidth: CGFloat = 360
+                    
+                    LazyVGrid(columns: columns, spacing: 20) {
+                        ForEach(categories.mainTopicsList, id: \.self) { title in
+                            if let topic = categories.mainTopics[title] {
+                                CategoryButton(topic: topic, selectedTopics: $selectedTopics, isReady: $isReady)
+                            }
+                            
+                        }
+                    }
+                    .frame(width: categoryWidth)
+                    .padding(.bottom)
+                }
+                let nextButtonWidth: CGFloat = 310
+                let nextButtonHeight: CGFloat = 25
+                if isReady{
+                    Button(action: {
+                        
+                        //TODO: 다음버튼 누르면 넘어가게 하기
+                    }) {
+                        Text("다음")
+                            .font(.headline)
+                            .foregroundColor(.black)
+                            .frame(width: nextButtonWidth, height: nextButtonHeight)
+                            .padding()
+                            .background(isReady ? Color("MainColor") : Color.gray)
+                            .clipShape(Capsule())
+                    }
+                    .disabled(selectedTopics.count==0)
+                }
+            }
+        }
+    }
+    
+    struct CategoryButton: View {
+        @ObservedObject var topic: mainTopic
+        @Binding var selectedTopics: [mainTopic]
+        @Binding var isReady: Bool
+        
+        let boxSize: CGFloat = 155
+        
+        var body: some View {
+            Button(action: {
+                if !(selectedTopics.count == 3 && !topic.isSelected){
+                    topic.isSelected.toggle()
+                    updateSelectedTopicList(topicList: &selectedTopics, tgt: topic)
+                }
+                isReady = (selectedTopics.count > 0 && selectedTopics.count < 4) ? true : false
+            }) {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(topic.isSelected ? Color("MainColor") : Color("CategoryColor"))
+                        .frame(width: boxSize, height: boxSize)
+                    
+                    VStack(spacing: 8) {
+                        Text(topic.emoji)
+                        Text(topic.title)
+                            .font(.headline)
+                            .foregroundColor(topic.isSelected ? .black : .white)
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                }
             }
         }
     }
 }
 
+func updateSelectedTopicList(topicList: inout [mainTopic], tgt: mainTopic){
+    if tgt.isSelected{
+        topicList.append(tgt)
+    }
+    else{
+        topicList.removeAll { $0.title == tgt.title }
+    }
+}
+
+
 #Preview{
     CategoryView(progress: 1.0 / 3.0)
 }
+
